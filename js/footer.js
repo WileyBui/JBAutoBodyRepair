0650d23be0789bc29581f60bc563a20ce2bc8eb2
Wiley Bui <17buih@gmail.com>
2019-01-01 03:06:40 -0600

    Added

---
+++ js/footer.js
@@ -0,0 +1,39 @@
+document.write(
+"<hr class='clearfix' style='visibility: hidden;'>" +
+"<footer>" +
+"	<div class='container'>" +
+"		<div class='row text-center'>" +
+"			<div class='col-12 col-md-4' id='contact'>" +
+"				<h5 class='text-uppercase font-weight-bold'>Contact</h5>" +
+"				<ul>" +
+"					<li><strong>Jackson Bui</strong></li>" +
+"				</ul>" +
+"				<a href='tel:612-806-5584' class='text-dark'>(612) 806-5584</a><br>" +
+"				<a href='mailto:jackbui17@gmail.com' class='text-dark' target='_blank'>JacksonBui17@gmail.com</a>" +
+"			</div>" +
+"			<hr class='clearfix w-100 d-md-none'>" +
+"			<div class='col-12 col-md-4'>" +
+"				<h5 class='text-uppercase font-weight-bold'>Location</h5>" +
+"				<strong>JB Auto Body Repair</strong><br>" +
+"				<a href='https://www.google.com/maps/dir//2613+Highway+13+West,+Burnsville,+MN+55337/@44.7737518,-93.3150212,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x87f63b0e2d39a76d:0x6fb39a7767f2bd2d!2m2!1d-93.3128325!2d44.7737518!3e0' class='text-dark' target='_blank'>" +
+"					2613 Highway 13 West<br>" +
+"					Burnsville, MN 55337<br>" +
+"				</a>" +
+"			</div>" +
+"			<hr class='clearfix w-100 d-md-none'>" +
+"			<div class='col-12 col-md-4' id='hour'>" +
+"				<h5 class='text-uppercase font-weight-bold'>Business Hours</h5>" +
+"				<ul>" +
+"					<li><strong>Mon-Fri</strong>: 9:00AM-6:00PM</li>" +
+"					<li><strong>Sat</strong>: 9:00AM-1:00PM</li>" +
+"					<li><strong>Sun</strong>: Closed</li>" +
+"				</ul>" +
+"			</div>" +
+"		</div>" +
+"	</div>" +
+"	<ul id='footer-copyright' class='text-center'>" +
+"		<li>Website by <a href='https://wileybui.com' class='font-weight-bold text-muted link' id='credit' target='_blank'>Wiley Bui</a>.</li>" +
+"		<li>&#169; 2019 JB Auto Body Repair. All Rights Reserved.</li>" +
+"	</ul>" +
+"</footer>"
+);
