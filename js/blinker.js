bb539c7c0d4609acaa53f0e7a3364106e9af6bff
Wiley Bui <17buih@gmail.com>
2019-01-02 23:48:17 -0600

    Added

---
+++ js/blinker.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$("#nav-contact").click(function(){$("#contact").addClass("blinker"),setTimeout(function(){$("#contact").removeClass("blinker")},1e3)}),$("#nav-hour").click(function(){$("#hour").addClass("blinker"),setTimeout(function(){$("#hour").removeClass("blinker")},1e3)})});
--- js/header.js
+++ js/header.js
@@ -1,19 +1,21 @@
 document.write(
-"<nav class='navbar fixed-top navbar-expand-md navbar-dark scrolling-navbar default-background-color'>" + 
-"	<div class='navbar-brand' style='width: 69vw'><a href='/'><div class='mobile-site-logo test'></div></a></div>" + 
-"	<button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>" + 
-"		<span class='navbar-toggler-icon'></span>" + 
-"	</button>" + 
-"	<div class='collapse navbar-collapse' id='navbarSupportedContent'>" + 
-"		<nav class='container-fluid'>" + 
-"			<ul class='navbar-nav mr-auto row align-items-center text-center text-uppercase'>" + 
-"				<li class='nav-item col-md-2'><a class='link' href='index.html'>Home</a></li>" + 
-"				<li class='nav-item col-md-3'><a class='link' href='#contact' id='nav-contact'>Contact</a></li>" + 
-"				<li class='nav-item col-0 col-md-2'><a href='/'><img src='images/site-logo.png' class='main-site-logo' /></a></li>" + 
-"				<li class='nav-item col-md-3'><a class='link' href='location.html'>Location</a></li>" + 
-"				<li class='nav-item col-md-2'><a class='link' href='#hour' id='nav-hour'>Business Hours</a></li>" + 
-"			</ul>" + 
-"		</nav>" + 
+"<nav class='navbar fixed-top navbar-expand-md navbar-dark scrolling-navbar default-background-color'>" +
+"	<div class='navbar-brand'>" +
+"		<a href='/' target='_blank' class='mobile-site-logo'></a>" +
+"		<button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>" +
+"			<span class='navbar-toggler-icon'></span>" +
+"		</button>" +
+"	</div>" +
+"	<div class='collapse navbar-collapse' id='navbarSupportedContent'>" +
+"		<nav class='container-fluid'>" +
+"			<ul class='navbar-nav mr-auto row align-items-center text-center text-uppercase'>" +
+"				<li class='nav-item col-md-2'><a class='link' href='/'>Home</a></li>" +
+"				<li class='nav-item col-md-3'><a class='link' href='#contact' id='nav-contact'>Contact</a></li>" +
+"				<li class='nav-item col-0 col-md-2'><a href='/'><img src='images/site-logo.png' class='main-site-logo' /></a></li>" +
+"				<li class='nav-item col-md-3'><a class='link' href='location.html'>Location</a></li>" +
+"				<li class='nav-item col-md-2'><a class='link' href='#hour' id='nav-hour'>Business Hours</a></li>" +
+"			</ul>" +
+"		</nav>" +
 "	</div>" +
 "</nav>"
 );
