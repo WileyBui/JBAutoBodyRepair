f6b1a4dbd15fb1ea4bbbd3ba53aa03d15ef8d856
Wiley Bui <buixx206@umn.edu>
2019-07-01 22:22:28 -0500

    Added

--- js/footer.js
+++ js/footer.js
@@ -22,11 +22,11 @@ document.write(
 "			</div>" +
 "			<hr class='clearfix w-100 d-md-none'>" +
 "			<div class='col-12 col-md-4' id='hour'>" +
-"				<h5 class='text-uppercase font-weight-bold'>Hours <font color='green'>Auto</font> | <font color='red'>Towing</font></h5>" +
+"				<h5 class='text-uppercase font-weight-bold'>Hours <font color='#3FF510'>Auto</font> | <font color='#E7263B'>Towing</font></h5>" +
 "				<ul>" +
-"					<li><strong>Mon-Fri</strong>: <font color='green'>9:00AM-6:00PM</font> | <font color='red'>9:00AM - 10:00PM</font></li>" +
-"					<li><strong>Sat</strong>: <font color='green'>9:00AM-1:00PM</font> | <font color='red'>9:00AM - 6:00PM</font></li>" +
-"					<li><strong>Sun</strong>: <font color='green'>Closed</font> | <font color='red'>Closed</font></li>" +
+"					<li><strong>Mon-Fri</strong>: <font color='#3FF510'>9:00AM-6:00PM</font> | <font color='#E7263B'>9:00AM - 10:00PM</font></li>" +
+"					<li><strong>Sat</strong>: <font color='#3FF510'>9:00AM-1:00PM</font> | <font color='#E7263B'>9:00AM - 6:00PM</font></li>" +
+"					<li><strong>Sun</strong>: <font color='#3FF510'>Closed</font> | <font color='#E7263B'>Closed</font></li>" +
 "				</ul>" +
 "			</div>" +
 "		</div>" +
